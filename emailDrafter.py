173a25e8448b537b3421769747bb2d69042205d5
arapoport918 <arapoport918@gmail.com>
2024-07-19 11:13:29 -0400

    Created scripts for 60 and 100 day emails

--- emailDrafter.py
+++ emailDrafter.py
@@ -13,11 +13,29 @@ def write_my_email(path, replacements, output_path):
 
     template.save(output_path)
 
-path = "15DayTEMPLATE.docx"
-replacements = {
+path15 = "15DayTEMPLATE.docx"
+replacements15 = {
     "CLIENT_NAME": "Allegra",
     "BRAND": "Some Brand Name"
     }
-output_path = "yayThisWorkedBetter.docx"
+output_path15 = "yayThisWorkedBetter.docx"
 
-write_my_email(path, replacements, output_path)
+path60 = "60DayTEMPLATE.docx"
+replacements60 = {
+    "ADNAME": "Allegra",
+    "CLIENT": "Brand for Testing",
+    "DATE": "July 19th, 2024"
+    }
+output_path60 = "yayThisWorkedfor60.docx"
+
+path100 = "100DayTEMPLATE.docx"
+replacements100 = {
+    "CLIENT_NAME": "Allegra's Famous Brand",
+    "CLIENT_CONTACT_NAMES": "Bryce, Trea, Nick, Alec",
+    "PROGRAM_TYPE": "affiliate"
+    }
+output_path100 = "yayThisWorkedfor100.docx"
+
+write_my_email(path15, replacements15, output_path15)
+write_my_email(path60, replacements60, output_path60)
+write_my_email(path100, replacements100, output_path100)
