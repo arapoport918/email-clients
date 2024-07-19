264e974cedfa7f9c108eacc60e845eabe2895d42
arapoport918 <arapoport918@gmail.com>
2024-07-19 10:53:39 -0400

    Basic loop to replace words set up, needs to be tested

--- emailDrafter.py
+++ emailDrafter.py
@@ -1,2 +1,15 @@
 import docx
 print(docx.__version__)
+
+
+def write_my_email(path, replacements, output_path):
+
+    #Load Document
+    template = Document(path)
+
+    for paragraph in template.paragraphs:
+        for key, value in replacements.items():
+            if key in pragraph.text:
+                paragraph.text = paragraph.text.replace(key, value)
+
+    template.save(output_path)
